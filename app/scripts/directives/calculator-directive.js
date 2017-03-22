76e01e297c5a3b26ce9b094c8342573ea4d35b86
David Smith <davidsmith2@gmail.com>
2017-03-22 12:59:03 -0400

    Angular.$eval -> $parse

--- app/scripts/directives/calculator-directive.js
+++ app/scripts/directives/calculator-directive.js
@@ -1,6 +1,6 @@
 angular.module('angularjsCalculatorApp')
 
-.directive('calculator', function(CalculatorService) {
+.directive('calculator', function($parse, CalculatorService) {
   return {
     restrict: 'E',
     templateUrl: 'views/directives/calculator-directive.html',
@@ -8,10 +8,11 @@ angular.module('angularjsCalculatorApp')
       $scope.re = /^[0-9\+*/-]+$/i;
       $scope.calculations = CalculatorService.getCalculations();
       $scope.calculate = function() {
+        var getOutput = $parse($scope.input);
         CalculatorService.addCalculation({
           date: new Date().getTime(),
           input: $scope.input,
-          output: $scope.$eval($scope.input)
+          output: getOutput()
         });
         $scope.input = '';
       };
